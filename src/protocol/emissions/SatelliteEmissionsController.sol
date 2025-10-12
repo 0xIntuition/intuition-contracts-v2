0432f62897c9674fe7e8b6d0fe8317a6838fe856
Kames <sudo@kames.me>
2025-10-12 10:13:52 -0700

    chore: use _msgSender()

--- src/protocol/emissions/SatelliteEmissionsController.sol
+++ src/protocol/emissions/SatelliteEmissionsController.sol
@@ -242,7 +242,7 @@ contract SatelliteEmissionsController is
         );
 
         if (msg.value > gasLimit) {
-            Address.sendValue(payable(msg.sender), msg.value - gasLimit);
+            Address.sendValue(payable(_msgSender()), msg.value - gasLimit);
         }
 
         emit UnclaimedEmissionsBridged(epoch, amount);
