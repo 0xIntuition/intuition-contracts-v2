a40fba384013aca774dcdc91c491ef2c6989effb
Kames <sudo@kames.me>
2025-10-27 11:26:13 -0700

    fix: production parameters

--- script/SetupScript.s.sol
+++ script/SetupScript.s.sol
@@ -19,18 +19,27 @@ import { LinearCurve } from "src/protocol/curves/LinearCurve.sol";
 import { OffsetProgressiveCurve } from "src/protocol/curves/OffsetProgressiveCurve.sol";
 
 abstract contract SetupScript is Script {
+    /// @dev The address of the transaction broadcaster.
+    address internal broadcaster;
+
+    /* =================================================== */
+    /*                   Config Constants                  */
+    /* =================================================== */
     uint256 public constant NETWORK_BASE = 8453;
     uint256 public constant NETWORK_BASE_SEPOLIA = 84_532;
     uint256 public constant NETWORK_INTUITION = 1155;
     uint256 public constant NETWORK_INTUITION_SEPOLIA = 13_579;
     uint256 public constant NETWORK_ANVIL = 31_337;
 
-    /* =================================================== */
-    /*                  Network Specific                   */
-    /* =================================================== */
+    uint256 internal constant ONE_DAY = 86_400;
+    uint256 internal constant TWO_WEEKS = ONE_DAY * 14;
 
-    /// @dev The address of the transaction broadcaster.
-    address internal broadcaster;
+    /// @dev Needed for the deterministic deployments.
+    bytes32 internal constant ZERO_SALT = bytes32(0);
+
+    /* =================================================== */
+    /*                   Smart Contracts                   */
+    /* =================================================== */
 
     Trust public trust;
     MultiVault public multiVault;
@@ -44,27 +53,29 @@ abstract contract SetupScript is Script {
     LinearCurve public linearCurve;
     OffsetProgressiveCurve public offsetProgressiveCurve;
 
-    // Setter Constants
-    uint256 internal constant ONE_DAY = 86_400;
-    uint256 internal constant TWO_WEEKS = ONE_DAY * 14;
+    /* =================================================== */
+    /*                  Critical Configs                   */
+    /* =================================================== */
 
-    /// @dev Needed for the deterministic deployments.
-    bytes32 internal constant ZERO_SALT = bytes32(0);
+    uint256 internal PROTOCOL_START_TIMESTAMP = 1762275600; // Tues November 4, 2025 12:00:00 EST || Wed November 5, 2025 00:00:00 KST
+    
+    uint256 internal TRUST_TOKEN_ONE_YEAR_EMISSIONS = 75_000_000 ether; // 75 million TRUST
+    
+    /* =================================================== */
+    /*                  Network Specific                   */
+    /* =================================================== */
 
     // MetaLayer Configurations
     address internal METALAYER_HUB_OR_SPOKE;
     uint32 internal BASE_METALAYER_RECIPIENT_DOMAIN;
     uint32 internal SATELLITE_METALAYER_RECIPIENT_DOMAIN;
     
-    uint156 internal PROTOCOL_START_TIMESTAMP;
-
     // General Config
     address internal ADMIN;
     address internal PROTOCOL_MULTISIG;
     address internal TRUST_TOKEN;
-    // address internal METALAYER_HUB_OR_SPOKE;
 
-    uint256 internal FEE_THRESHOLD = 1e17;
+    uint256 internal FEE_THRESHOLD = 1e18; // 1 Trust
     uint256 internal MIN_DEPOSIT = 1e15; // 0.001 Trust
 
     // Atom Config
@@ -148,29 +159,44 @@ abstract contract SetupScript is Script {
         info("ChainID:", block.chainid);
         info("Broadcasting:", broadcaster);
 
-        if (block.chainid == NETWORK_INTUITION_SEPOLIA) {
+        if (block.chainid == NETWORK_BASE_SEPOLIA) {
+            PROTOCOL_START_TIMESTAMP = 0;
+            TRUST_TOKEN = 0xA54b4E6e356b963Ee00d1C947f478d9194a1a210;
+            ADMIN = vm.envAddress("BASE_SEPOLIA_ADMIN_ADDRESS");
+            PROTOCOL_MULTISIG = vm.envOr("BASE_SEPOLIA_PROTOCOL_MULTISIG", ADMIN);
+
+            // MetaLayer Intuition Hub
+            METALAYER_HUB_OR_SPOKE = 0x007700aa28A331B91219Ffa4A444711F0D9E57B5;
+            SATELLITE_METALAYER_RECIPIENT_DOMAIN = 13_579;
+
+            // Emissions Controller Config
+            EMISSIONS_START_TIMESTAMP = PROTOCOL_START_TIMESTAMP;
+            EMISSIONS_LENGTH = TWO_WEEKS;
+            EMISSIONS_REDUCTION_BASIS_POINTS = 1000; // 10%
+            EMISSIONS_REDUCTION_CLIFF = 26;
+            EMISSIONS_PER_EPOCH = TRUST_TOKEN_ONE_YEAR_EMISSIONS / EMISSIONS_REDUCTION_CLIFF;
+        } else if (block.chainid == NETWORK_INTUITION_SEPOLIA) {
             PROTOCOL_START_TIMESTAMP = 0;
             TRUST_TOKEN = 0xDE80b6EE63f7D809427CA350e30093F436A0fe35; // Wrapped Trust
             ADMIN = vm.envAddress("INTUITION_SEPOLIA_ADMIN_ADDRESS");
             PROTOCOL_MULTISIG = vm.envOr("INTUITION_SEPOLIA_PROTOCOL_MULTISIG", ADMIN);
 
-            BASE_METALAYER_RECIPIENT_DOMAIN = 84_532;
-
             // Timelock Config
             TIMELOCK_MIN_DELAY = 60 minutes;
 
             // MetaLayer Config
+            BASE_METALAYER_RECIPIENT_DOMAIN = 84_532;
             METALAYER_HUB_OR_SPOKE = 0x007700aa28A331B91219Ffa4A444711F0D9E57B5;
 
             // General Config
-            MIN_DEPOSIT = 1e15; // 0.001 Trust
+            MIN_DEPOSIT = 1e18; // 0.001 Trust
 
             // Atom Config
-            ATOM_CREATION_PROTOCOL_FEE = 1e15; // 0.001 Trust (Fixed Cost)
+            ATOM_CREATION_PROTOCOL_FEE = 1e18; // 0.001 Trust (Fixed Cost)
             ATOM_WALLET_DEPOSIT_FEE = 100; // 1% of assets after fixed costs (Percentage Cost)
 
             // Triple Config
-            TRIPLE_CREATION_PROTOCOL_FEE = 1e15; // 0.001 Trust (Fixed Cost)
+            TRIPLE_CREATION_PROTOCOL_FEE = 1e18; // 0.001 Trust (Fixed Cost)
             ATOM_DEPOSIT_FRACTION_FOR_TRIPLE = 90; // 0.9% (Percentage Cost)
 
             // TrustBonding Config
@@ -179,29 +205,41 @@ abstract contract SetupScript is Script {
             BONDING_SYSTEM_UTILIZATION_LOWER_BOUND = 4000; // 50%
             BONDING_PERSONAL_UTILIZATION_LOWER_BOUND = 2500; // 25%
 
-            // CoreEmissionsController Config
+            // Emissions Controller Config
             EMISSIONS_START_TIMESTAMP = PROTOCOL_START_TIMESTAMP;
-            EMISSIONS_LENGTH = ONE_DAY;
+            EMISSIONS_LENGTH = TWO_WEEKS;
             EMISSIONS_REDUCTION_BASIS_POINTS = 1000; // 10%
-            EMISSIONS_REDUCTION_CLIFF = 4; // 1 epoch
-            EMISSIONS_PER_EPOCH = 1000 ether;
+            EMISSIONS_REDUCTION_CLIFF = 26; // 1 epoch
+            EMISSIONS_PER_EPOCH = TRUST_TOKEN_ONE_YEAR_EMISSIONS / EMISSIONS_REDUCTION_CLIFF;
+        } else if (block.chainid == NETWORK_BASE) {
+            // Global Config
+            TRUST_TOKEN = 0x6cd905dF2Ed214b22e0d48FF17CD4200C1C6d8A3;
+            ADMIN = 0xBc01aB3839bE8933f6B93163d129a823684f4CDF;
+
+            // MetaLayer Config
+            SATELLITE_METALAYER_RECIPIENT_DOMAIN = 1155;
+            METALAYER_HUB_OR_SPOKE = 0xE12aaF1529Ae21899029a9b51cca2F2Bc2cfC421;
+
+            // Emissions Controller Config
+            EMISSIONS_START_TIMESTAMP = PROTOCOL_START_TIMESTAMP;
+            EMISSIONS_LENGTH = TWO_WEEKS;
+            EMISSIONS_REDUCTION_BASIS_POINTS = 1000; // 10%
+            EMISSIONS_REDUCTION_CLIFF = 26;
+            EMISSIONS_PER_EPOCH = TRUST_TOKEN_ONE_YEAR_EMISSIONS / EMISSIONS_REDUCTION_CLIFF;
         } else if (block.chainid == NETWORK_INTUITION) {
-            PROTOCOL_START_TIMESTAMP = 1762275600; // Tues November 4, 2025 12:00:00 EST || Wed November 5, 2025 00:00:00 KST
             TRUST_TOKEN = 0x81cFb09cb44f7184Ad934C09F82000701A4bF672;
             ADMIN = 0xbeA18ab4c83a12be25f8AA8A10D8747A07Cdc6eb;
             PROTOCOL_MULTISIG = 0x4B44BE80739fb11544Dd835a0f55580Ad19Ce0F3;
 
-            // MetaLayer Config
-            BASE_METALAYER_RECIPIENT_DOMAIN = 8453;
-
             // Timelock Config
             TIMELOCK_MIN_DELAY = 5 minutes;
 
-            // MetaLayer Intuition Spoke
+            // MetaLayer Config
+            BASE_METALAYER_RECIPIENT_DOMAIN = 8453;
             METALAYER_HUB_OR_SPOKE = 0x375135fe908dD62f3C7939FA4e65bf41Da721AB9;
 
             // General Config
-            MIN_DEPOSIT = 1e18; // 0.1 Trust
+            MIN_DEPOSIT = 1e18; // Trust
 
             // Atom Config
             ATOM_CREATION_PROTOCOL_FEE = 1e18; // 1 Trust (Fixed Cost)
@@ -217,49 +255,22 @@ abstract contract SetupScript is Script {
             BONDING_SYSTEM_UTILIZATION_LOWER_BOUND = 4000; // 40%
             BONDING_PERSONAL_UTILIZATION_LOWER_BOUND = 2500; // 25%
 
-            // CoreEmissionsController Config
+            // Emissions Controller Config
             EMISSIONS_START_TIMESTAMP = PROTOCOL_START_TIMESTAMP;
             EMISSIONS_LENGTH = TWO_WEEKS;
             EMISSIONS_REDUCTION_BASIS_POINTS = 1000; // 10%
-            EMISSIONS_REDUCTION_CLIFF = 26; // 26 x two week epochs = 1 year
-            EMISSIONS_PER_EPOCH = 75_000_000 ether / EMISSIONS_REDUCTION_CLIFF; // 75_000_000 TRUST/year |
-                // 2884615384615384615384615 wei/epoch | 2_884_615.384615384615384615 TRUST/epoch
-        } else if (block.chainid == NETWORK_BASE_SEPOLIA) {
-            TRUST_TOKEN = 0xA54b4E6e356b963Ee00d1C947f478d9194a1a210;
-            ADMIN = vm.envAddress("BASE_SEPOLIA_ADMIN_ADDRESS");
-            PROTOCOL_MULTISIG = vm.envOr("BASE_SEPOLIA_PROTOCOL_MULTISIG", ADMIN);
-
-            // MetaLayer Intuition Hub
-            METALAYER_HUB_OR_SPOKE = 0x007700aa28A331B91219Ffa4A444711F0D9E57B5;
-            SATELLITE_METALAYER_RECIPIENT_DOMAIN = 13_579;
-        } else if (block.chainid == NETWORK_BASE) {
-            // Global Config
-            PROTOCOL_START_TIMESTAMP = 1762275600;
-            TRUST_TOKEN = 0x6cd905dF2Ed214b22e0d48FF17CD4200C1C6d8A3;
-            ADMIN = 0xBc01aB3839bE8933f6B93163d129a823684f4CDF;
-
-            // MetaLayer Config
-            METALAYER_HUB_OR_SPOKE = 0xE12aaF1529Ae21899029a9b51cca2F2Bc2cfC421;
-            SATELLITE_METALAYER_RECIPIENT_DOMAIN = 1155;
-
-            // Emissions Controller Config
-            EMISSIONS_START_TIMESTAMP = PROTOCOL_START_TIMESTAMP;
-            EMISSIONS_LENGTH = ONE_DAY;
-            EMISSIONS_REDUCTION_BASIS_POINTS = 1000; // 10%
-            EMISSIONS_REDUCTION_CLIFF = 4; // 1 epoch
-            EMISSIONS_PER_EPOCH = 1000 ether;
-
+            EMISSIONS_REDUCTION_CLIFF = 26;
+            EMISSIONS_PER_EPOCH = TRUST_TOKEN_ONE_YEAR_EMISSIONS / EMISSIONS_REDUCTION_CLIFF;
         } else if (block.chainid == NETWORK_ANVIL) {
             ADMIN = vm.envAddress("ANVIL_ADMIN_ADDRESS");
             TRUST_TOKEN = vm.envOr("ANVIL_TRUST_TOKEN", address(0));
             PROTOCOL_MULTISIG = vm.envOr("ANVIL_PROTOCOL_MULTISIG", ADMIN);
 
-            BASE_METALAYER_RECIPIENT_DOMAIN = 11_111;
-
             // Timelock Config
             TIMELOCK_MIN_DELAY = 60 minutes;
 
             // MetaLayer Config
+            BASE_METALAYER_RECIPIENT_DOMAIN = 11_111;
             METALAYER_HUB_OR_SPOKE = 0x007700aa28A331B91219Ffa4A444711F0D9E57B5;
 
             // General Config
