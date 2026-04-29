513736e4f1fc4614249a7541c06b05f57b548068
mihailo-maksa <mihajlomaksa9+@gmail.com>
2026-04-29 19:34:42 +0200

    chore: remove MultiVault from the v1.0.2 deploy script for Intuition mainnet

--- script/intuition/v1.0.2/DeployCoreUpgradeImplementations.s.sol
+++ script/intuition/v1.0.2/DeployCoreUpgradeImplementations.s.sol
@@ -9,7 +9,6 @@ import {
 import { ProxyAdmin } from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
 import { UpgradeableBeacon } from "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
 import { SetupScript } from "script/SetupScript.s.sol";
-import { MultiVault } from "src/protocol/MultiVault.sol";
 import { TrustBonding } from "src/protocol/emissions/TrustBonding.sol";
 import { OffsetProgressiveCurve } from "src/protocol/curves/OffsetProgressiveCurve.sol";
 import { AtomWallet } from "src/protocol/wallet/AtomWallet.sol";
@@ -32,8 +31,6 @@ contract DeployCoreUpgradeImplementations is SetupScript {
     error UnsupportedChain();
 
     /// @dev Mainnet proxy addresses (Intuition, chain 1155)
-    address internal constant MULTIVAULT_PROXY = 0x6E35cF57A41fA15eA0EaE9C33e751b01A784Fe7e;
-    address internal constant MULTIVAULT_PROXY_ADMIN = 0x1999faD6477e4fa9aA0FF20DaafC32F7B90005C8;
     address internal constant TRUST_BONDING_PROXY = 0x635bBD1367B66E7B16a21D6E5A63C812fFC00617;
     address internal constant TRUST_BONDING_PROXY_ADMIN = 0xF10FEE90B3C633c4fCd49aA557Ec7d51E5AEef62;
     address internal constant OFFSET_CURVE_PROXY = 0x23afF95153aa88D28B9B97Ba97629E05D5fD335d;
@@ -42,7 +39,6 @@ contract DeployCoreUpgradeImplementations is SetupScript {
     address internal constant SATELLITE_EC_PROXY = 0x73B8819f9b157BE42172E3866fB0Ba0d5fA0A5c6;
     address internal constant SATELLITE_EC_PROXY_ADMIN = 0xdF60D18E86F3454309aD7734055843F7ee5f30a3;
 
-    MultiVault public multiVaultImplementation;
     TrustBonding public trustBondingImplementation;
     OffsetProgressiveCurve public offsetProgressiveCurveImplementation;
     AtomWallet public atomWalletImpl;
@@ -64,7 +60,6 @@ contract DeployCoreUpgradeImplementations is SetupScript {
 
         console2.log("");
         console2.log("DEPLOYMENT COMPLETE: =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+");
-        contractInfo("MultiVault Implementation", address(multiVaultImplementation));
         contractInfo("TrustBonding Implementation", address(trustBondingImplementation));
         contractInfo("OffsetProgressiveCurve Implementation", address(offsetProgressiveCurveImplementation));
         contractInfo("AtomWallet Implementation", address(atomWalletImpl));
@@ -74,9 +69,6 @@ contract DeployCoreUpgradeImplementations is SetupScript {
     }
 
     function _deployImplementations() internal {
-        // multiVaultImplementation = new MultiVault();
-        // info("MultiVault Implementation", address(multiVaultImplementation));
-
         trustBondingImplementation = new TrustBonding();
         info("TrustBonding Implementation", address(trustBondingImplementation));
 
@@ -95,16 +87,6 @@ contract DeployCoreUpgradeImplementations is SetupScript {
     function _logUpgradeCalldata() internal view {
         console2.log("");
         console2.log("UPGRADE CALLDATA: =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+");
-
-        // ProxyAdmin.upgradeAndCall(proxy, newImpl, 0x) for each TransparentUpgradeableProxy
-        // console2.log("");
-        // console2.log("MultiVault upgrade (target: ProxyAdmin %s):", MULTIVAULT_PROXY_ADMIN);
-        // console2.logBytes(
-        //     abi.encodeCall(
-        //         ProxyAdmin.upgradeAndCall,
-        //         (ITransparentUpgradeableProxy(MULTIVAULT_PROXY), address(multiVaultImplementation), "")
-        //     )
-        // );
 
         console2.log("");
         console2.log("TrustBonding upgrade (target: ProxyAdmin %s):", TRUST_BONDING_PROXY_ADMIN);
